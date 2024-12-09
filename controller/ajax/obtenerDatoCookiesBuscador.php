8e8d8d291867f2f581f77704d8a28e1864bc5251
Miguel Local <miguelsaladus@gmail.com>
2024-12-09 03:19:39 +0100

    eliminammosss

--- controller/ajax/obtenerDatoCookiesBuscador.php
+++
@@ -1,7 +0,0 @@
-<?php
-   
-    require_once "../../model/pokemon.php";
-    $resultadoBusqueda=Pokemon::obtenerNombrePokemonAlmacenarCookie($_GET['valorBuscadorPokemon']);
-
-    echo $resultadoBusqueda;
-?>
